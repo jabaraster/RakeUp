ef7ee0d1963f0b39f9e5b4c3d3db2fde067ccfd7
Jabara <jabaraster@gmail.com>
2012-12-25 08:47:17 +0900

    mainte.

--- src/main/java/jabara/rakeup/entity/EEntry_.java
+++ src/main/java/jabara/rakeup/entity/EEntry_.java
@@ -6,11 +6,11 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2012-12-24T01:44:50.405+0900")
+@Generated(value="Dali", date="2012-12-25T08:21:56.049+0900")
 @StaticMetamodel(EEntry.class)
 public class EEntry_ extends EntityBase_ {
+	public static volatile SingularAttribute<EEntry, String> title;
 	public static volatile SingularAttribute<EEntry, String> text;
 	public static volatile ListAttribute<EEntry, EKeyword> keywords;
 	public static volatile SingularAttribute<EEntry, ESource> source;
-	public static volatile SingularAttribute<EEntry, String> title;
 }
